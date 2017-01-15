7097b7a8ddacc99efa66d3acbdca81c6da8327f3
Leo White <leo@lpw25.net>
2017-01-15 13:57:04 +0000

    Add Definitions.v

---
+++ Definitions.v
@@ -0,0 +1,9 @@
+(************************************************
+ *          Row Subtyping - Definitions         *
+ *                 Leo White                    *
+ ************************************************)
+
+Set Implicit Arguments.
+Require Import LibLN.
+Implicit Types x : var.
+Implicit Types X : var.
